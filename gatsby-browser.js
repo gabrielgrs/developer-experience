6657e40a65d13cafd28927ddbb593e611b467f70
Gabriel R <grxgabriel@gmail.com>
2020-08-14 17:13:53 -0300

    :bug: fix gatsby-browser improt

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SWUpdater from './src/components/SWUpdate'
+import SWUpdater from './src/components/SWUpdater'
 
 function onServiceWorkerUpdateReady() {
   const root = document.createElement('div')
